84894abd6729007494e0c1b2eaaa736d794803b9
shaimaaalshaikh <alshikshaimaa@gmail.com>
2023-09-04 18:57:43 +0200

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("مرحبا بك في موقعي انا اسمي شيماء وسوف احسب درجتك");
+const grade = parseInt(prompt("ادخل درجتك"));
+console.log(grade);
+
+if(grade >=90 && grade <=100 ) {
+    console.log("لقد حصلت على امتياز");
+}
+
+ else if(grade >=80 && grade <=89 ) {
+    console.log("لقد حصلت على جيد جدا ");
+}
+
+else if(grade >=70 && gradث<=79 ) {
+    console.log("لقد حصلت على جيد ");
+}
